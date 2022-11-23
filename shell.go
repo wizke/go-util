33fb4154fcb7ad2c05638c6eda5345b233256fa1
KennyWang <kenny.wang@example.com>
2022-11-23 13:43:35 +0800

    update

--- shell.go
+++ shell.go
@@ -2,12 +2,20 @@ package util
 
 import (
 	"os/exec"
+	"runtime"
 )
 
 // ExecShell 阻塞式的执行外部shell命令的函数,等待执行完毕并返回所有输出
-func ExecShell(s string) (string, error) {
+func ExecShell(s string) (out []byte, err error) {
 	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
-	cmd := exec.Command("/bin/bash", "-c", s)
-	out, err := cmd.CombinedOutput()
-	return string(out), err
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		cmd = exec.Command("/bin/bash", "-c", s)
+		out, err = cmd.CombinedOutput()
+	case "windows":
+		cmd = exec.Command("cmd.exe", "/c", s)
+		out, err = cmd.CombinedOutput()
+	}
+	return out, err
 }
