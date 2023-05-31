7940384f761581f99f63c2823d99ac55b06f2f1c
KennyWang <winstonkenny@foxmail.com>
2023-05-31 16:06:43 +0800

    fix bug when zero

--- num2str.go
+++ num2str.go
@@ -6,11 +6,11 @@ import (
 )
 
 func FormatBytes(bytes uint64) string {
-	const unit = 1024
-	if bytes < unit {
-		return fmt.Sprintf("%.0f B", bytes)
-	}
 	fBytes := float64(bytes)
+	const unit = 1024
+	if fBytes < unit {
+		return fmt.Sprintf("%.0f B", fBytes)
+	}
 	exp := math.Floor(math.Log(fBytes) / math.Log(unit))
 	units := []string{"B", "KB", "MB", "GB", "TB", "PB"}
 	return fmt.Sprintf("%.1f %s", float64(bytes)/math.Pow(unit, exp), units[int(exp)])
